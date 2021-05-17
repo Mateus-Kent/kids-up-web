af9e0a67c7b1a5349fdf0672d9031fc11c4f8e7e
Mateus-Kent <benriba2003@gmail.com>
2021-05-17 19:07:12 -0300

    Cria servidor

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const server = express();
+
+
+server.use(express.static("public"))
+
+server.get('/', (resquest , response) =>{
+  return response.sendFile(__dirname + "/views/index.html");
+})
+
+server.listen(3000 , () => console.log('rodando'))
