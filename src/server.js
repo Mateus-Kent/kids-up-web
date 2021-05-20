716652ab3d7a72ae7b4dc5fc1cff1c826a5e9b2a
Mateus-Kent <benriba2003@gmail.com>
2021-05-19 21:33:12 -0300

    Adiciona pagina de edit-profile

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 
+server.use(express.static("public"));
 
-server.use(express.static("public"))
-
-server.use(routes)
-server.listen(3000 , () => console.log('rodando'))
+server.use(routes);
+server.listen(3000, () => console.log("rodando"));
