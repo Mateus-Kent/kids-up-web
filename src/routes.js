715fd85ca314236152fca36d43f4ac0c6a084438
Mateus-Kent <benriba2003@gmail.com>
2021-05-21 23:02:01 -0300

    Adiciona autenticação de senha

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,22 @@ const routes = express.Router();
 
 const views = __dirname + "/views/" 
 
+const parent = []
+
 routes.get('/', (req , res) =>res.render(views + 'index'));
 
 routes.get('/signup', (req , res) => res.render(views + 'signup'));
+
+routes.post('/signup', (req , res) => {
+  
+  parent.push({
+    email: req.body.email,
+    password: req.body.password,
+    username: req.body.username,
+  })
+  console.log(parent);
+  return res.redirect('/');
+});
 
 routes.get('/login', (req , res) => res.render(views + 'login'));
 
